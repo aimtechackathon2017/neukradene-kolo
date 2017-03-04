53c0a91d683a58f309c70515ee7471ac33455845
esler <esler@users.noreply.github.com>
2017-03-04 23:17:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "x_nucleo_iks01a1.h"
 
 const int ACC_TRESHOLD = 100;
+const int THIEF_WARMDOWN = 10 * 60 * 10; // ten minutes
 
 /* Instantiate the expansion board */
 static X_NUCLEO_IKS01A1 *mems_expansion_board = X_NUCLEO_IKS01A1::Instance(D14, D15);
@@ -15,20 +16,12 @@ DigitalOut myled(LED1);
 Serial pc(USBTX, USBRX);
 
 /* Serial connection to sigfox modem */
-//Serial modem(PA_9, PA_10);
+Serial modem(PA_9, PA_10);
 //Serial modem(PA_2, PA_3);
-Serial modem(PC_10, PC_11);
+//Serial modem(PC_10, PC_11);
 
 
 int locked = 0;
-
-// fce prototypes
-void sertmout();
-bool modem_command_check_ok(char * command);
-void modem_setup();
-void txData(uint8_t btn);
-
-bool ser_timeout = false;
  
 int main() {
   int32_t axes[3];
@@ -37,22 +30,20 @@ int main() {
   int32_t sum[3] = {0,0,0};
   int count = 0;
   float diff;
-  
-  wait(3);
-  
-    pc.printf("is readable?\r\n");
-  
-    /* first clear serial data buffers */
-    while(modem.readable()) modem.getc();
-    
-    pc.printf("modem readable\r\n");
-  modem_setup();
+  int warmup = 3; // how many diffs to ignore
+  int warmdown = 0;
   
   while(1) {
     if (mybutton == 0) { // Button is pressed
       myled = !myled; // Toggle the LED state
-            
-      locked = locked == 0 ? 1 : 0; 
+      
+      if (locked == 0) {
+          locked = 1;
+          printf("Zamknul jsi kolo!\r\n");
+      } else {
+          locked = 0;
+          printf("Odemknul jsi kolo!\r\n");
+      }
     }
     
 //    pc.printf("Is locked? %d\r\n", locked);
@@ -82,10 +73,13 @@ int main() {
         
 //        printf("LSM6DS0 [acc/mg]:      %f, %f, %f\r\n", avg[0], avg[1], avg[2]);
         
-        modem_command_check_ok("AT");
-        
-        if (diff > ACC_TRESHOLD) {
+        if (warmup-- < 0 && diff > ACC_TRESHOLD && warmdown-- == 0) {
             printf("\r\nCovece kradou ti KOLO!\r\n");
+            warmdown = THIEF_WARMDOWN;
+            
+            // send to sigfox
+//            modem.printf("AT$SF=BABADEDA\n");
+            pc.printf("AT$SF=BABADEDA\r\n");
         }
         
         last_avg[0] = avg[0];
@@ -96,89 +90,5 @@ int main() {
     printf(".");
     wait(0.1);
   }
-}
-
-
-
-/* TX data over Sigfox */
-//void txData (uint8_t btn)
-//{
-//
-//    float    tAvg;
-//    char     command[32];
-//    sensor.Sense();
-//    tAvg = sensor.GetAverageTemp();
-//    char temperature[6] ="";
-//    sprintf(temperature, "%3.1f", tAvg);
-//    for(int i = 0; i < 5; i++)
-//        if(temperature[i]==0) temperature[i] = ' ';
-//    sprintf(command, "AT$SF=0142544e%x20%x%x%x%x%x43,2,0\n", btn+48, temperature[0],temperature[1],temperature[2],temperature[3],temperature[4]);
-//    pc.printf("Sending pressed button %d and temperature %s C over Sigfox.\n", btn, temperature);
-//    pc.printf("using modem command: %s", command);
-//    modem_command_check_ok(command);
-//    LED_1 = 1;
-//    LED_2 = 1;
-//}
-
-void modem_setup()
-{
-    /* Reset to factory defaults */
-    if(modem_command_check_ok("AT&F")) {
-        pc.printf("Factory reset succesfull\r\n");
-    } else {
-        pc.printf("Factory reset TD120x failed\r\n");
-    }
-    /* Disable local echo */
-    modem.printf("ATE0\n");
-    if(modem_command_check_ok("ATE0")) {
-        pc.printf("Local echo disabled\r\n");
-    }
-    /* Write to mem */
-    if(modem_command_check_ok("AT&W")) {
-        pc.printf("Settings saved!\r\n");
-    }
-}
-
-/* ISR for serial timeout */
-void sertmout()
-{
-    pc.printf("serial timeouted!\r\n");
-    ser_timeout = true;
-}
-
-bool modem_command_check_ok(char * command)
-{
-    /* first clear serial data buffers */
-    while(modem.readable()) modem.getc();
-    /* Timeout for response of the modem */
-    Timeout tmout;
-    ser_timeout = false;
-    /* Buffer for incoming data */
-    char responsebuffer[6];
-    /* Flag to set when we get 'OK' response */
-    bool ok = false;
-    bool error = false;
-    /* Print command to TD120x */
-    modem.printf(command);
-    /* Newline to activate command */
-    modem.printf("\n");
-    /* Wait untill serial feedback, max 3 seconds before timeout */
-    tmout.attach(&sertmout, 3.0);
-    while(!modem.readable()&& ser_timeout == false);
-    while(!ok && !ser_timeout && !error) {
-        if(modem.readable()) {
-            for(int i = 0; i < 5; i++) {
-                responsebuffer[i] = responsebuffer[i+1];
-            }
-            responsebuffer[5] = modem.getc();
-            if(responsebuffer[0] == '\r' && responsebuffer[1] == '\n' && responsebuffer[2] == 'O' && responsebuffer[3] == 'K' && responsebuffer[4] == '\r' && responsebuffer[5] == '\n' ) {
-                ok = true;
-            } else if(responsebuffer[0] == '\r' && responsebuffer[1] == '\n' && responsebuffer[2] == 'E' && responsebuffer[3] == 'R' && responsebuffer[4] == 'R' && responsebuffer[5] == 'O' ) {
-                error = true;
-            }
-        }
-    }
-    tmout.detach();
-    return ok;
 }
  
